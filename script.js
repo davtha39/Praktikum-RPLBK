40424b628ab3dd11380240c86f3b31db9ffd81ce
daviathallah <daviathallah@gmail.com>
2021-09-02 21:22:18 +0700

    mod 1

---
+++ script.js
@@ -0,0 +1,8 @@
+setTimeout( () => console.log("Kode baris pertama"), 3000);
+setTimeout( () => console.log("Kode baris kedua"), 1000);
+setTimeout( () => console.log("Kode baris ketiga"), 2000);
+/* Output
+Kode baris kedua
+Kode baris ketiga
+kode baris pertama
+*/
